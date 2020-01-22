a89262400be2b79c13bb59e3ff4691a55358b075
Carmel Braga <cpbrz5@mail.missouri.edu>
2020-01-22 19:51:06 -0300

    Initial Commit

---
+++ Text To SpeechTests/Text_To_SpeechTests.swift
@@ -0,0 +1,34 @@
+//
+//  Text_To_SpeechTests.swift
+//  Text To SpeechTests
+//
+//  Created by Felipe Braga on 22/01/20.
+//  Copyright © 2020 Carmel Braga. All rights reserved.
+//
+
+import XCTest
+@testable import Text_To_Speech
+
+class Text_To_SpeechTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
